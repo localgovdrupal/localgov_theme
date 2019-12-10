7eb7c7711abbd39d033593ee62c79a5914d4e003
chris stevens <cjs78@hotmail.co.uk>
2019-12-10 11:02:02 +0000

    added sass linter to gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
-var scsslint = require('gulp-scss-lint');
 var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
 var changed = require('gulp-changed');
-var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
 var del = require('del');
 var imagemin = require('gulp-imagemin');
@@ -17,47 +14,59 @@ var versionAppend = require('gulp-version-append');
 // clean the dist folder
 gulp.task('clean', function(){
 
-	del.sync(['dist/'])
+  del.sync(['dist/'])
 
 });
- 
-gulp.task('scss-lint', function() {
-  return gulp.src('/scss/*.scss')
-    .pipe(scsslint());
+
+// Lint and report on scss
+
+var postcss = require('gulp-postcss');
+var reporter = require('postcss-reporter');
+var stylelint = require('stylelint');
+var scss = require("postcss-scss");
+
+gulp.task('lint:css', function() {
+    return gulp.src('./src/assets/scss/**/*.scss')
+        .pipe(postcss(
+            [
+                stylelint({ /* options */ }),
+                reporter({ clearMessages: true })
+            ],
+            {
+                syntax: scss
+            }
+        ));
 });
 
 // compile sass and autoprefix
 gulp.task('sass', function(){
 
-	var onError = function(err) {
+  var onError = function(err) {
         notify.onError({
-                    title:    "Gulp",
-                    subtitle: "Sass compiler error",
-                    message:  "Error: <%= error.message %>",
-                    sound: "Hero"
-                })(err);
+          title:    "Gulp",
+          subtitle: "Sass compiler error",
+          message:  "Error: <%= error.message %>",
+          sound: "Hero"
+        })(err);
 
         this.emit('end');
     };
 
-	return gulp.src('./src/assets/scss/**/*.scss')
-		.pipe(plumber({errorHandler: onError}))
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer())
-    // .pipe(cleanCSS())
-		.pipe(gulp.dest('./dist/assets/css/'))
-    .pipe(browserSync.stream())
+  return gulp.src('./src/assets/scss/**/*.scss')
+
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./dist/assets/css/'))
     
 });
 
-
 // pipe js to dist/
 gulp.task('js', function(){
-	// if specific order is required
-  // return gulp.src(['./lib/file3.js', './lib/file1.js', './lib/file2.js'])
-	return gulp.src('./src/assets/js/*.js')
-		.pipe(concat('main.js'))
-		.pipe(gulp.dest('./dist/assets/js/'))
+
+  return gulp.src('./src/assets/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./dist/assets/js/'))
 
 });
 
@@ -65,69 +74,34 @@ gulp.task('js', function(){
 // optimise images
 gulp.task('images', function(){
 
-	return gulp.src('./src/assets/img/**/*')
-		.pipe(changed('./dist/assets/img/**/*'))
-		.pipe(imagemin({
-			progressive: true
-		}))
-		.pipe(gulp.dest('./dist/assets/img/'))
+  return gulp.src('./src/assets/img/**/*')
+    .pipe(changed('./dist/assets/img/**/*'))
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(gulp.dest('./dist/assets/img/'))
 
 });
 
 
-// pipe fonts to dist/
+// pipe fonts to dist
 gulp.task('fonts', function(){
 
-	return gulp.src('./src/assets/fonts/**/*')
-		.pipe(gulp.dest('./dist/assets/fonts/'))
+  return gulp.src('./src/assets/fonts/**/*')
+    .pipe(gulp.dest('./dist/assets/fonts/'))
 
 });
 
 
-// pipe inc folder to dist/
-gulp.task('html', function(){
-
-	return gulp.src(['./src/inc/**/*'])
-		.pipe(changed('./dist/inc/'))
-		.pipe(gulp.dest('./dist/inc/'))
-		.pipe(browserSync.stream())
-
-});
-
-
-// pipe php files to dist/
-gulp.task('php', function(){
-
-	return gulp.src(['./src/*.php'])
-		.pipe(changed('./dist/'))
-		.pipe(gulp.dest('./dist/'))
-		.pipe(browserSync.stream())
-
-});
-
-
-// BrowserSync reload all browsers
-gulp.task('browsersync-reload', function () {
-
-  browserSync.reload();
-
-});
-
-// watch for changes and initialise browserSync
+// watch for changes
 gulp.task('watch', function(){
 
-  browserSync.init({
-		proxy: 'localhost:8888/client/croydon/Theme/croydon-theme/dist'
-  });
-
-  gulp.watch('src/assets/scss/**/*.scss',['sass']);
-  gulp.watch('src/assets/js/**/*.js',['js','browsersync-reload']);
-  gulp.watch('src/assets/img/*',['images','browsersync-reload']);
-  gulp.watch('src/assets/fonts/**/*',['fonts','browsersync-reload']);
-  gulp.watch('src/inc/**/*.html',['html']);
-  gulp.watch('src/*.php',['php']);
-	
+  gulp.watch('src/assets/scss/**/*.scss',['sass'], ['scss-lint']);
+  gulp.watch('src/assets/js/**/*.js',['js']);
+  gulp.watch('src/assets/img/*',['images']);
+  gulp.watch('src/assets/fonts/**/*',['fonts']);
+  
 });
 
 // build entire dist folder
-gulp.task('build',['clean','sass','js','images','fonts','html','php']);
+gulp.task('build',['clean','lint:css','sass','js','images','fonts']);
